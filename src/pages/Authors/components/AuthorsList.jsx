70810ff31e2beac24f068afccb1b792a29695233
Stanislav Trunov <107206323+stanlettee@users.noreply.github.com>
2025-09-21 17:07:13 +0100

    authors images

--- src/pages/Authors/components/AuthorsList.jsx
+++ src/pages/Authors/components/AuthorsList.jsx
@@ -1,26 +1,33 @@
+import naomi from '../../../images/authors/naomi.png'
+import emma from '../../../images/authors/emma.png'
+import andrii from '../../../images/authors/andrii.png'
+import max from '../../../images/authors/max.png'
+import tony from '../../../images/authors/tony.png'
+import tailor from '../../../images/authors/tailor.png'
+
 export const AuthorsList = () => {
     return (
          <ul className="authors-list">
-            <li className="authors-item"><img src="/" alt="Author"/></li>
-            <li className="authors-item"><img src="/" alt="Author"/></li>
-            <li className="authors-item"><img src="/" alt="Author"/></li>
-            <li className="authors-item"><img src="/" alt="Author"/></li>
-            <li className="authors-item"><img src="/" alt="Author"/></li>
-            <li className="authors-item"><img src="/" alt="Author"/></li>
-            <li className="authors-item"><img src="/" alt="Author"/></li>
-            <li className="authors-item"><img src="/" alt="Author"/></li>
-            <li className="authors-item"><img src="/" alt="Author"/></li>
-            <li className="authors-item"><img src="/" alt="Author"/></li>
-            <li className="authors-item"><img src="/" alt="Author"/></li>
-            <li className="authors-item"><img src="/" alt="Author"/></li>
-            <li className="authors-item"><img src="/" alt="Author"/></li>
-            <li className="authors-item"><img src="/" alt="Author"/></li>
-            <li className="authors-item"><img src="/" alt="Author"/></li>
-            <li className="authors-item"><img src="/" alt="Author"/></li>
-            <li className="authors-item"><img src="/" alt="Author"/></li>
-            <li className="authors-item"><img src="/" alt="Author"/></li>
-            <li className="authors-item"><img src="/" alt="Author"/></li>
-            <li className="authors-item"><img src="/" alt="Author"/></li>
+            <li className="authors-item"><img src={naomi} alt="Author"/><p className="auhtors-name">Naomi</p></li>
+            <li className="authors-item"><img src={andrii} alt="Author"/><p className="auhtors-name">Andrii</p></li>
+            <li className="authors-item"><img src={emma} alt="Author"/><p className="auhtors-name">Emma</p></li>
+            <li className="authors-item"><img src={max} alt="Author"/><p className="auhtors-name">Max</p></li>
+            <li className="authors-item"><img src={tony} alt="Author"/><p className="auhtors-name">Tony</p></li>
+            <li className="authors-item"><img src={tailor} alt="Author"/><p className="auhtors-name">Tailor</p></li>
+            <li className="authors-item"><img src={naomi} alt="Author"/><p className="auhtors-name">Naomi</p></li>
+            <li className="authors-item"><img src={emma} alt="Author"/><p className="auhtors-name">Emma</p></li>
+            <li className="authors-item"><img src={andrii} alt="Author"/><p className="auhtors-name">Andrii</p></li>
+            <li className="authors-item"><img src={max} alt="Author"/><p className="auhtors-name">Max</p></li>
+            <li className="authors-item"><img src={tony} alt="Author"/><p className="auhtors-name">Tony</p></li>
+            <li className="authors-item"><img src={tailor} alt="Author"/><p className="auhtors-name">Tailor</p></li>
+            <li className="authors-item"><img src={naomi} alt="Author"/><p className="auhtors-name">Naomi</p></li>
+            <li className="authors-item"><img src={emma} alt="Author"/><p className="auhtors-name">Emma</p></li>
+            <li className="authors-item"><img src={andrii} alt="Author"/><p className="auhtors-name">Andrii</p></li>
+            <li className="authors-item"><img src={max} alt="Author"/><p className="auhtors-name">Max</p></li>
+            <li className="authors-item"><img src={tailor} alt="Author"/><p className="auhtors-name">Tailor</p></li>
+            <li className="authors-item"><img src={tony} alt="Author"/><p className="auhtors-name">Tony</p></li>
+            <li className="authors-item"><img src={andrii} alt="Author"/><p className="auhtors-name">Andrii</p></li>
+            <li className="authors-item"><img src={tailor} alt="Author"/><p className="auhtors-name">Tailor</p></li>
         </ul>
     )
 }
