b523ebb9daf1252165ef2d12457a4ec86a03d8cc
Stanislav Trunov <107206323+stanlettee@users.noreply.github.com>
2025-09-21 16:34:40 +0100

    authors

--- src/pages/Authors/Authors.jsx
+++ src/pages/Authors/Authors.jsx
@@ -1,1 +1,19 @@
-export const Authors = () => {}
+import { Header } from '../../components/Header/Header'
+import { Footer } from '../../components/Footer/footer'
+import { AuthorsList } from './components/AuthorsList'
+
+export const Authors = () => {
+    return (<>
+        <Header />
+        <main>
+            <section className="authors">
+                <div className="container authors-container">
+                    <h2 className="authors-title">Authors</h2>
+                    <AuthorsList />
+                    <button className="authors-button">Load More</button>
+                </div>
+            </section>
+        </main>
+        <Footer />
+    </>)
+}
