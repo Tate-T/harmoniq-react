ab8ac73e3be7ad052af8d12a03392a941f148fc0
Ostik <117195748+Ostik1220@users.noreply.github.com>
2025-09-21 18:05:53 +0300

    added components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import { ArticlesList } from './components/ArticlesList/ArticlesList';
+import { Footer } from './components/Footer/footer';
+import { Header } from './components/Header/Header';
+import { Articles } from './pages/Articles/Articles';
+import { ArticlesPage } from './pages/ArticlesPage/ArticlesPage';
+import { Auth } from './pages/Auth/Auth';
+import { AuthorProfilePage } from './pages/AuthorProfilePage/AuthorProfilePage';
+import { Authors } from './pages/Authors/Authors';
+import { CreateArticlePage } from './pages/CreateArticlePage/CreateArticlePage';
+import { Home } from './pages/Home/Home';
+import { UserProfile } from './pages/UserProfile/UserProfile';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Home />
+      <Articles />
+      <ArticlesPage />
+      <Auth />
+      <AuthorProfilePage />
+      <Authors />
+      <CreateArticlePage />
+      <UserProfile />
+      <ArticlesList />
+      <Footer />
     </div>
   );
 }
