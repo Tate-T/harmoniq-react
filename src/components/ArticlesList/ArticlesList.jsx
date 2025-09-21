1470fac8ee76c90b4e7fa780ff69a1a4f2c4d867
artemanoh <61391756+artemanoh@users.noreply.github.com>
2025-09-21 18:50:53 +0300

    зробив розмітку для ArticlesList

--- src/components/ArticlesList/ArticlesList.jsx
+++ src/components/ArticlesList/ArticlesList.jsx
@@ -1,1 +1,57 @@
-export const ArticlesList = () => {}
+import { Header } from "../../components/Header/Header";
+import { Footer } from "../../components/Footer/footer";
+
+
+
+const ArticleCard = ({ title, description, image, name }) => {
+  return (
+    <article>
+      <img src={image} alt={title} />
+      <h2>{name}</h2>
+      <h3>{title}</h3>
+      <p>{description}</p>
+      <button>Learn more</button>
+      <button></button>
+    </article>
+  );
+};
+
+export const ArticlesList = () => {
+  const articles = [
+    {
+      id: 1,
+      title: "1",
+      name: "Category A",
+      description: "test.",
+      image: "./",
+    },
+    {
+      id: 2,
+      title: "2",
+      name: "Popular",
+      description: "test.",
+      image: "./",
+    },
+  ];
+
+  return (
+    <>
+      <Header />
+      <div>
+        <h1>Articles</h1>
+        </div>
+      <div>
+        {articles.map((article) => (
+          <ArticleCard
+            key={article.id}
+            title={article.title}
+            description={article.description}
+            image={article.image}
+            name={article.name}
+          />
+        ))}
+      </div>
+      <Footer/>
+    </>
+  );
+};
