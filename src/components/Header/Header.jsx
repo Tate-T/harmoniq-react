b2ffca9caa9f654dfdabbf67d51ac6af2e2474a8
Ostik <117195748+Ostik1220@users.noreply.github.com>
2025-09-21 18:43:50 +0300

    hedaer-component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,1 +1,18 @@
-export const Header = () => {}
+import logo from '../../images/logo.png';
+
+export const Header = () => {
+    return <>
+    <header>
+        <img src={logo} alt="harmoniq-logo" />
+        <div>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Articles</a></li>
+                <li><a href="">Creators</a></li>
+                <li><a href="">Log in</a></li>
+            </ul>
+            <button type="button">Join now</button>
+        </div>
+    </header>
+    </>
+}
