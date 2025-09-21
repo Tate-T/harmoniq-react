1d2bfb10621bafdc4d642c1e8c341e8235f93c18
LidiiaPopadiuk <lidiiadolfin@gmail.com>
2025-09-21 17:49:09 +0200

    created HomePage

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,1 +1,47 @@
-export const Home = () => {}
+import { Header } from '../../components/Header'
+import { Footer } from '../../components/Footer'
+
+export const Home = () => {
+    return <>
+        <Header />
+        <main>
+            <section>
+                <div>
+                    <h1>Find your harmony in community</h1>
+                    <div>
+                        <button>Go to Articles</button>
+                        <button>Register</button>
+                    </div>
+                </div>
+                <img src="" alt="" />
+            </section>
+            <section>
+                <div>
+                    <div>
+                        <h2>About us</h2>
+                        <p>Harmoniq is a mindful publishing platform dedicated to mental health and well-being. We bring together writers, thinkers, and readers who believe that open, thoughtful stories can heal, inspire, and connect. Whether you're here to share your journey or learn from others — this is your space to  slow down, reflect, and grow.</p>
+                    </div>
+                    <img src="" alt="" />
+                </div>
+                <div>
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                </div>
+            </section>
+            <section>
+                <div>
+                    <h2>Popular Articles</h2>
+                    <div>
+                        <p>Go to all Articles</p>
+                        //svg
+                    </div>
+                </div>
+                <div>
+                    //?<div></div>
+                </div>
+            </section>
+            <section></section>
+        </main>
+        <Footer />
+    </>
+}
