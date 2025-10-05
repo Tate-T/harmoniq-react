b996be15176b6ba2621ec568f7434834da98d911
CupheadUltra <129087076+CupheadUltra@users.noreply.github.com>
2025-10-05 18:43:09 +0300

    container

---
+++ src/pages/AuthorProfilePage/articlesData.jsx
@@ -0,0 +1,43 @@
+
+export const articles = [
+  {
+    author: "Clark",
+    title: "When Anxiety Feels Like a Room With No Doors",
+    description: "10 advices how meditations can help you feeling better",
+    image: "https://picsum.photos/400/250?random=1",
+  },
+  {
+    author: "Debby",
+    title: "The Quiet Power of Doing Nothing",
+    description:
+      "In a culture obsessed with productivity, embracing rest can be an act of resistance — and...",
+    image: "https://picsum.photos/400/250?random=2",
+  },
+  {
+    author: "Max",
+    title: "Mindful Mornings: 5-Minute Rituals to Start Your Day with Calm",
+    description:
+      "Simple, science-backed practices that can gently shift your mood and focus before the day begins.",
+    image: "https://picsum.photos/400/250?random=3",
+  },
+  {
+    author: "Debby",
+    title: "The Quiet Power of Doing Nothing",
+    description:
+      "In a culture obsessed with productivity, embracing rest can be an act of resistance — and...",
+    image: "https://picsum.photos/400/250?random=4",
+  },
+  {
+    author: "Clark",
+    title: "When Anxiety Feels Like a Room With No Doors",
+    description: "10 advices how meditations can help you feeling better",
+    image: "https://picsum.photos/400/250?random=5",
+  },
+  {
+    author: "Max",
+    title: "Mindful Mornings: 5-Minute Rituals to Start Your Day with Calm",
+    description:
+      "Simple, science-backed practices that can gently shift your mood and focus before the day begins.",
+    image: "https://picsum.photos/400/250?random=6",
+  },
+];
