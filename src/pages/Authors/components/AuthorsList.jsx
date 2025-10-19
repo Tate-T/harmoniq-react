682406ac0c2dc96b5f2664a98566f3e61facdd8a
Stanislav Trunov <107206323+stanlettee@users.noreply.github.com>
2025-10-19 15:40:31 +0100

    header and authors

--- src/pages/Authors/components/AuthorsList.jsx
+++ src/pages/Authors/components/AuthorsList.jsx
@@ -9,26 +9,26 @@ import styles from '../Authors.module.css'
 export const AuthorsList = () => {
     return (
          <ul className={styles.authorsList}>
-            <li className={styles.authorsItem}><img className={styles.img} src={naomi} alt="Author"/><p className="auhtors-name">Naomi</p></li>
-            <li className={styles.authorsItem}><img className={styles.img} src={andrii} alt="Author"/><p className="auhtors-name">Andrii</p></li>
-            <li className={styles.authorsItem}><img className={styles.img} src={emma} alt="Author"/><p className="auhtors-name">Emma</p></li>
-            <li className={styles.authorsItem}><img className={styles.img} src={max} alt="Author"/><p className="auhtors-name">Max</p></li>
-            <li className={styles.authorsItem}><img className={styles.img} src={tony} alt="Author"/><p className="auhtors-name">Tony</p></li>
-            <li className={styles.authorsItem}><img className={styles.img} src={tailor} alt="Author"/><p className="auhtors-name">Tailor</p></li>
-            <li className={styles.authorsItem}><img className={styles.img} src={naomi} alt="Author"/><p className="auhtors-name">Naomi</p></li>
-            <li className={styles.authorsItem}><img className={styles.img} src={emma} alt="Author"/><p className="auhtors-name">Emma</p></li>
-            <li className={styles.authorsItem}><img className={styles.img} src={andrii} alt="Author"/><p className="auhtors-name">Andrii</p></li>
-            <li className={styles.authorsItem}><img className={styles.img} src={max} alt="Author"/><p className="auhtors-name">Max</p></li>
-            <li className={styles.authorsItem}><img className={styles.img} src={tony} alt="Author"/><p className="auhtors-name">Tony</p></li>
-            <li className={styles.authorsItem}><img className={styles.img} src={tailor} alt="Author"/><p className="auhtors-name">Tailor</p></li>
-            <li className={styles.authorsItem}><img className={styles.img} src={naomi} alt="Author"/><p className="auhtors-name">Naomi</p></li>
-            <li className={styles.authorsItem}><img className={styles.img} src={emma} alt="Author"/><p className="auhtors-name">Emma</p></li>
-            <li className={styles.authorsItem}><img className={styles.img} src={andrii} alt="Author"/><p className="auhtors-name">Andrii</p></li>
-            <li className={styles.authorsItem}><img className={styles.img} src={max} alt="Author"/><p className="auhtors-name">Max</p></li>
-            <li className={styles.authorsItem}><img className={styles.img} src={tailor} alt="Author"/><p className="auhtors-name">Tailor</p></li>
-            <li className={styles.authorsItem}><img className={styles.img} src={tony} alt="Author"/><p className="auhtors-name">Tony</p></li>
-            <li className={styles.authorsItem}><img className={styles.img} src={andrii} alt="Author"/><p className="auhtors-name">Andrii</p></li>
-            <li className={styles.authorsItem}><img className={styles.img} src={tailor} alt="Author"/><p className="auhtors-name">Tailor</p></li>
+            <li className={styles.authorsItem}><img className={styles.img} src={naomi} alt="Author"/><p className={styles.name}>Naomi</p></li>
+            <li className={styles.authorsItem}><img className={styles.img} src={andrii} alt="Author"/><p className={styles.name}>Andrii</p></li>
+            <li className={styles.authorsItem}><img className={styles.img} src={emma} alt="Author"/><p className={styles.name}>Emma</p></li>
+            <li className={styles.authorsItem}><img className={styles.img} src={max} alt="Author"/><p className={styles.name}>Max</p></li>
+            <li className={styles.authorsItem}><img className={styles.img} src={tony} alt="Author"/><p className={styles.name}>Tony</p></li>
+            <li className={styles.authorsItem}><img className={styles.img} src={tailor} alt="Author"/><p className={styles.name}>Tailor</p></li>
+            <li className={styles.authorsItem}><img className={styles.img} src={naomi} alt="Author"/><p className={styles.name}>Naomi</p></li>
+            <li className={styles.authorsItem}><img className={styles.img} src={emma} alt="Author"/><p className={styles.name}>Emma</p></li>
+            <li className={styles.authorsItem}><img className={styles.img} src={andrii} alt="Author"/><p className={styles.name}>Andrii</p></li>
+            <li className={styles.authorsItem}><img className={styles.img} src={max} alt="Author"/><p className={styles.name}>Max</p></li>
+            <li className={styles.authorsItem}><img className={styles.img} src={tony} alt="Author"/><p className={styles.name}>Tony</p></li>
+            <li className={styles.authorsItem}><img className={styles.img} src={tailor} alt="Author"/><p className={styles.name}>Tailor</p></li>
+            <li className={styles.authorsItem}><img className={styles.img} src={naomi} alt="Author"/><p className={styles.name}>Naomi</p></li>
+            <li className={styles.authorsItem}><img className={styles.img} src={emma} alt="Author"/><p className={styles.name}>Emma</p></li>
+            <li className={styles.authorsItem}><img className={styles.img} src={andrii} alt="Author"/><p className={styles.name}>Andrii</p></li>
+            <li className={styles.authorsItem}><img className={styles.img} src={max} alt="Author"/><p className={styles.name}>Max</p></li>
+            <li className={styles.authorsItem}><img className={styles.img} src={tailor} alt="Author"/><p className={styles.name}>Tailor</p></li>
+            <li className={styles.authorsItem}><img className={styles.img} src={tony} alt="Author"/><p className={styles.name}>Tony</p></li>
+            <li className={styles.authorsItem}><img className={styles.img} src={andrii} alt="Author"/><p className={styles.name}>Andrii</p></li>
+            <li className={styles.authorsItem}><img className={styles.img} src={tailor} alt="Author"/><p className={styles.name}>Tailor</p></li>
         </ul>
     )
 }
