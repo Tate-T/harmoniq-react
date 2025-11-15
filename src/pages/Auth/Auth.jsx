0cfbaf3eddeb8949c2b843f829c1f1f69d3cf3d0
Ostik <117195748+Ostik1220@users.noreply.github.com>
2025-11-15 18:30:17 +0200

    register work

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -2,16 +2,23 @@ import { Header } from "../../components/Header/Header";
 import { Footer } from "../../components/Footer/footer";
 import { Register } from "./RegisterForm";
 import { Login } from "./LoginForm";
+import { Component } from "react";
 
 
-export const Auth = () => {
-    const isRegistred = false;
-    return <>
-    <Header/>
-    <main>
-    {isRegistred === false ? <Login/> : <Register/>}
-    </main>
-    <Footer/>
-    </>
-
+export default class Auth extends Component {
+  state = {
+    users: this.props.users
+  };
+    render() {
+        const isRegistred = true
+        return (
+            <>
+                <Header />
+                <main>
+                    {isRegistred === false ? <Login /> : <Register  users={this.state.users}/>}
+                </main>
+                <Footer />
+            </>
+        )
+    }
 }
--- src/pages/Auth/RegisterForm.jsx
+++ src/pages/Auth/RegisterForm.jsx
@@ -1,46 +1,85 @@
-import styles from './Auth.module.css'
-import eye from '../../images/auth/eye-crossed.svg'
-import { Component } from 'react'
+import styles from './Auth.module.css';
+import eye from '../../images/auth/eye-crossed.svg';
+import { Component } from 'react';
 
 export class Register extends Component {
-    state = {
-        username: '',
-        email: '',
-        password: ''
-    }
+  state = {
+    username: '',
+    email: '',
+    password: '',
+    users: this.props.users
+  };
 
-    infoCollect = () => {
-        this.setState((PrevSate) => {
-            return 
-        })
-    }
-    
-    render() {
-        return <form className={styles.registerForm}>
-            <h2 className={styles.name}>Register</h2>
-            <p className={styles.p}>Join our community of mindfulness and wellbeing!</p>
-            <ul className={styles.list}>
-                <li className={styles.item}>
-                    <p className={styles.label}>Enter your name</p>
-                    <input type="text" className={styles.input} placeholder="Max"/>
-                </li>
-                <li className={styles.item}>
-                    <p className={styles.label}>Enter your email address</p>
-                    <input type="text" className={styles.input} placeholder="email@gmail.com"/>
-                </li>
-                <li className={styles.item}>
-                    <p className={styles.label}>Create a strong password</p>
-                    <input type="text" className={styles.input} placeholder="*****"/>
-                    <img src={eye} alt="" className={styles.eye}/>
-                </li>
-                <li className={styles.item}>
-                    <p className={styles.label}>Repeat your password</p>
-                    <input type="text" className={styles.input} placeholder="*****"/>
-                    <img src={eye} alt="" className={styles.eye}/>
-                </li>
-            </ul>
-            <button className={styles.btn} type="submit">Create account</button>
-            <p className={styles.sugtestion}>Already have an account? <a href="" className={styles.link}>Log in</a></p>
-        </form>
-    }
+  
+
+  collectInfo = event => {
+    const message = this.state.users.includes((user)=>{
+        if (user.email === this.state.email){ 
+           console.log("User with this email already exists");
+        } else {
+            console.log(user);
+        }
+    })
+    event.preventDefault();
+    this.setState(
+      {
+        email: event.target.elements.email.value,
+        password: event.target.elements.password.value,
+        username: event.target.elements.username.value,
+      }
+    );
+        event.target.elements.email.value = '';
+    event.target.elements.password.value = '';
+    event.target.elements.username.value = '';
+    console.log(message);
+  };
+
+
+
+  render() {
+    return (
+      <form className={styles.registerForm} onSubmit={this.collectInfo}>
+        <h2 className={styles.name}>Register</h2>
+        <p className={styles.p}>
+          Join our community of mindfulness and wellbeing!
+        </p>
+        <ul className={styles.list}>
+          <li className={styles.item}>
+            <p className={styles.label}>Enter your name</p>
+            <input name="username" type="username" className={styles.input} placeholder="Max" />
+          </li>
+          <li className={styles.item}>
+            <p className={styles.label}>Enter your email address</p>
+            <input
+              name="email"
+              type="email"
+              className={styles.input}
+              placeholder="email@gmail.com"
+            />
+          </li>
+          <li className={styles.item}>
+            <p className={styles.label}>Create a strong password</p>
+            <input             
+            name='password'
+ type="password" className={styles.input} placeholder="*****" />
+            <img src={eye} alt="" className={styles.eye} />
+          </li>
+          <li className={styles.item}>
+            <p className={styles.label}>Repeat your password</p>
+            <input type="password" className={styles.input} placeholder="*****" />
+            <img src={eye} alt="" className={styles.eye} />
+          </li>
+        </ul>
+        <button className={styles.btn} type="submit">
+          Create account
+        </button>
+        <p className={styles.sugtestion}>
+          Already have an account?{' '}
+          <a href="" className={styles.link}>
+            Log in
+          </a>
+        </p>
+      </form>
+    );
+  }
 }
